623f173b779251371e826bd40ffd45c37c763c1b
Alex yip <sz423242122@gmail.com>
2023-12-05 21:18:56 +0800

    feat: update code for pc

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,82 +1,58 @@
-import { Box, styled } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { AnimateStep } from "../App";
 import P1 from "../assets/imgs/p1.png";
 import P2 from "../assets/imgs/p2.png";
 import P3 from "../assets/imgs/p3.png";
+import P11 from "../assets/imgs/p1_1.svg";
+import P12 from "../assets/imgs/p1_2.svg";
+import P21 from "../assets/imgs/p2_1.png";
+import P22 from "../assets/imgs/p2_2.png";
+import P31 from "../assets/imgs/p3_1.png";
+import P32 from "../assets/imgs/p3_2.svg";
+import Logo2 from "../assets/imgs/logo2.svg";
 
 const CardItem = styled(Grid)(() => ({
   "&.card": {
+    flex: 1,
+    position: "relative",
     cursor: "pointer",
+    width: "calc(33% -24px)",
     height: "510px",
     minHeight: "510px",
     perspective: "1000px",
-    position: "relative",
-    ".front": {
-      display: "flex",
-      borderRadius: "24px",
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-      textAlign: "center",
-      justifyContent: "center",
-      alignItems: "center",
-      position: "absolute",
-      height: "100%",
-      width: "100%",
-      backfaceVisibility: "hidden",
-      transformStyle: "preserve-3d",
-      transition: "ease-in-out 600ms",
-    },
-    ".back": {
-      display: "flex",
-      borderRadius: "24px",
-      backgroundPosition: "center",
-      backgroundSize: "cover",
-      textAlign: "center",
-      justifyContent: "center",
-      alignItems: "center",
-      position: "absolute",
-      height: "100%",
-      width: "100%",
-      backfaceVisibility: "hidden",
-      transformStyle: "preserve-3d",
-      transition: "ease-in-out 600ms",
-    },
+    transformStyle: "preserve-3d",
+    transition: "ease-in-out 600ms",
+  },
+  ".front,.back": {
+    display: "flex",
+    borderRadius: "24px",
+    overflow: "hidden",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    left: 0,
+    top: 0,
+    height: "100%",
+    width: "100%",
+    backfaceVisibility: "hidden",
   },
   ".front": {
     backgroundSize: "cover",
     overflow: "hidden",
-    "&:before": {
-      position: "absolute",
-      display: "block",
-      content: `''`,
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      background: `linear-gradient(135deg, adjust-hue(hsl(222,80%,50%), -20deg), hsl(222,80%,50%))`,
-      opacity: 0.25,
-      zIndex: -1,
-    },
-    "&..card:hover": {
-      transform: "rotateY(180deg)",
-    },
-    "&..card:nth-child(even):hover": {
-      transform: "rotateY(-180deg)",
-    },
   },
   ".back": {
-    background: "#fff",
-    transform: "rotateY(-180deg)",
-    "&..card:hover": {
-      transform: "rotateY(0deg)",
-    },
-    "&..card:nth-child(even)": {
-      transform: "rotateY(180deg)",
-    },
-    "&..card:nth-child(even):hover": {
-      transform: "rotateY(0deg)",
-    },
+    background: "#181818",
+    padding: "48px 0",
+    transform: "rotateY(180deg)",
+    flexFlow: "column nowrap",
+    justifyContent: "space-between",
+  },
+  "&.card:hover": {
+    transform: "rotateY(180deg)",
   },
 }));
 const Cards = ({ animateStep }: { animateStep: AnimateStep }) => {
@@ -85,24 +61,106 @@ const Cards = ({ animateStep }: { animateStep: AnimateStep }) => {
       title: "Bounce Bit",
       text: "Unlock the Power of Bitcoin Ecosystem",
       bg: P1,
+      children: (
+        <>
+          <img
+            style={{
+              width: "155px",
+            }}
+            src={P11}
+            alt=""
+          />
+          <img
+            style={{
+              marginTop: "5px",
+              width: "78px",
+            }}
+            src={P12}
+            alt=""
+          />
+        </>
+      ),
     },
     {
       title: "Bounce Auction",
       text: "Auction as a Service",
       bg: P2,
+      children: (
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+          }}
+        >
+          <img
+            style={{
+              position: "relative",
+              width: "100%",
+              maxHeight: "300px",
+            }}
+            src={P21}
+            alt=""
+          />
+          <img
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate3D(-50%, -50%, 0)",
+              width: "274px",
+            }}
+            src={P22}
+            alt=""
+          />
+        </Box>
+      ),
     },
     {
       title: "Bounce Box",
       text: "You Defi Dreams All in One Box",
       bg: P3,
+      children: (
+        <Box
+          sx={{
+            position: "relative",
+            width: "100%",
+            display: "flex",
+            justifyContent:'center',
+            paddingBottom: "20px",
+          }}
+        >
+          <img
+            style={{
+              position: "relative",
+              width: "187px",
+            }}
+            src={P31}
+            alt=""
+          />
+          <img
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate3D(-50%, -50%, 0)",
+              width: "58px",
+            }}
+            src={P32}
+            alt=""
+          />
+        </Box>
+      ),
     },
   ];
   return (
     <Box
       sx={{
         position: "absolute",
-        top: "203px",
-        bottom: "65px",
+        display: "flex",
+        flexFlow: "row nowrap",
+        justifyContent: "center",
+        alignItems: "flex-end",
+        bottom: "60px",
         left: "50%",
         transform:
           animateStep < AnimateStep.allLeave
@@ -111,42 +169,90 @@ const Cards = ({ animateStep }: { animateStep: AnimateStep }) => {
         transition: "all 1.6s",
         width: "100%",
         overflow: "hidden",
+        padding: "0 40px",
+        margin: "0 auto",
+        height: "100%",
       }}
     >
-      <Grid
-        container
-        spacing={8}
-        direction={"row"}
-        sx={{
-          width: "100%",
-          padding: "0 40px",
-          margin: "0 auto",
-          height: "100%",
-        }}
-      >
-        {contents.map((item, index) => {
-          return (
-            <CardItem xs={4} key={index} className="card">
+      {contents.map((item, index) => {
+        return (
+          <CardItem key={index} className="card">
+            <Box className={"front"}>
               <Box
-                className={"front"}
                 sx={{
-                  color: "#fff",
+                  width: "100%",
+                  height: "100%",
+                  background: `url(${item.bg}) no-repeat center center / cover`,
+                  display: "flex",
+                  flexFlow: "column nowrap",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  overflow: "hidden",
                 }}
               >
-                {item.title}0000
+                <img src={Logo2} alt="" />
+                <Typography
+                  sx={{
+                    position: "absolute",
+                    bottom: "44px",
+                    left: "0",
+                    width: "100%",
+                    textAlign: "center",
+                    fontSize: "18px",
+                    color: "#fff",
+                  }}
+                >
+                  {item.title}
+                </Typography>
               </Box>
+            </Box>
+            <Box className={"back"}>
               <Box
-                className={"back"}
                 sx={{
-                  color: "#fff",
+                  width: "100%",
                 }}
               >
-                {item.title}1111
+                <Typography
+                  component={"p"}
+                  sx={{
+                    color: "#fff",
+                    width: "100%",
+                    fontSize: "40px",
+                    textAlign: "left",
+                    marginBottom: "14px",
+                    padding: "0 42px",
+                  }}
+                >
+                  {item.title}
+                </Typography>
+                <Typography
+                  component={"p"}
+                  sx={{
+                    fontSize: "18px",
+                    textAlign: "left",
+                    width: "100%",
+                    color: "#fff",
+                    padding: "0 42px",
+                  }}
+                >
+                  {item.text}
+                </Typography>
               </Box>
-            </CardItem>
-          );
-        })}
-      </Grid>
+              <Box
+                sx={{
+                  width: "100%",
+                  display: "flex",
+                  flexFlow: "column nowrap",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                {item.children}
+              </Box>
+            </Box>
+          </CardItem>
+        );
+      })}
     </Box>
   );
 };
